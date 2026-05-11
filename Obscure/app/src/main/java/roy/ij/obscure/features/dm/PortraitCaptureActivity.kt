7568c3061d1ea34b6cab54f326070561d4973def
IJ Roy <141866152+ij-roy@users.noreply.github.com>
2026-05-11 20:07:08 +0530

    Logo Updated

--- Obscure/app/src/main/java/roy/ij/obscure/features/dm/PortraitCaptureActivity.kt
+++ Obscure/app/src/main/java/roy/ij/obscure/features/dm/PortraitCaptureActivity.kt
@@ -1,6 +1,13 @@
 package roy.ij.obscure.features.dm
 
+import android.os.Bundle
+import android.view.WindowManager
 import com.journeyapps.barcodescanner.CaptureActivity
 
 // Empty subclass that we can reference in the IntentIntegrator
-class PortraitCaptureActivity : CaptureActivity()
+class PortraitCaptureActivity : CaptureActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE) // ✅
+        super.onCreate(savedInstanceState)
+    }
+}
